1ec46478076291e8dd3d898984d55e941975a689
Saranya <145927243+Saranyabanerjee@users.noreply.github.com>
2025-08-17 04:37:10 +0530

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -8,7 +8,7 @@ public class StudentManager {
     }
 
     public void displayStudents() {
-        System.out.println("Roll No\tName\tMarks\tGrade");
+        System.out.println("Roll No\tName\tMarks\tGrade"); 
         for (Student s : students) {
             System.out.println(s.getRollNumber() + "\t" + s.getName() + "\t" + s.getMarks() + "\t" + s.getGrade());
         }
